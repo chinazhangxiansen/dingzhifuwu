0550e8359c49409540334e35fde273978eab237e
zhangxiansen <1248123371@qq.com>
2020-08-20 10:14:47 +0800

    新增H5的一个分开的url并修改配置文件yml中的数据库连接信息

---
+++ src/main/java/com/pangmutou/education/controller/IndexH5Controller.java
@@ -0,0 +1,27 @@
+package com.pangmutou.education.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("web/h5/")
+public class IndexH5Controller {
+
+    @RequestMapping("index")
+    public String index(Model model){
+        return "front/h5/index.html";
+    }
+
+    @RequestMapping("star-teacher")
+    public String teacher(Model model){
+        return "front/h5/star-star-teacher.html";
+    }
+
+    @RequestMapping("reference-room")
+    public String book(Model model){
+        return "front/h5/reference-room.html";
+    }
+
+
+}
