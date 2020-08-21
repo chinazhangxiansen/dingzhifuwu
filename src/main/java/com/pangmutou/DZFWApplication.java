7a0d928284c8772860bf88f942e62f08c5b43ca3
zhangxiansen <1248123371@qq.com>
2020-08-21 22:59:44 +0800

    对springboot打包方式的修改以及对前端手机端以及PC端的自动识别跳转的修改

--- src/main/java/com/pangmutou/DZFWApplication.java
+++ src/main/java/com/pangmutou/DZFWApplication.java
@@ -3,15 +3,21 @@ package com.pangmutou;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @MapperScan("com.pangmutou.**.mapper")
 @SpringBootApplication
-public class DZFWApplication {
+public class DZFWApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DZFWApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DZFWApplication.class);
+    }
 }
